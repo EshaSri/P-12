cd012193a43a6455eba822f9f25c14cf8268caf4
EshaSri <100853196+EshaSri@users.noreply.github.com>
2022-03-24 17:02:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,32 +47,32 @@ var select_sprites = Math.round(random(1,3));
 
   
    //if (frameCount % 50 == 0) {
-     if (select_sprites == 1) {
-       createApples();
+     //if (select_sprites == 1) {
+      // createApples();
     //} else if (select_sprites == 2) {
      // createOrange();
     //}else {
      // createRed();
     //}
-   }
+   //}
 
-  //if (frameCount % 80 == 0) {
+  if (frameCount % 80 == 0) {
      if (select_sprites == 1) {
-     // createApples();
-    //} else if (select_sprites == 2) {
+      createApples();
+    } else if (select_sprites == 2) {
       createOrange();
-    //}
+    }
    }
 
   // if (frameCount / 80 == 0) {
-     if (select_sprites == 1) {
+     //if (select_sprites == 1) {
        //createApples();
      //} else if (select_sprites == 2) {
-      createOrange();
+     // createOrange();
     //}else {
      // createRed();
     //}
-    }
+    //}
 
    //if (frameCount % 80 = 0) {
      //if (select_sprites == 1) {
